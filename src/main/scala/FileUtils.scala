65e90471d0aab9d00b5ba105a1fc87cd3b8f2b9a
rossputin <ross@bheap.co.uk>
2011-07-07 21:23:52 +0100

    formatting improvement

--- src/main/scala/FileUtils.scala
+++ src/main/scala/FileUtils.scala
@@ -3,7 +3,7 @@ package com.bheap.scalautils
 object FileUtils {
   import java.io.FileWriter
   import ResourceUtils._
-  def writeFile(fileName:String, content:String) = 
+  def writeFile(fileName: String, content: String) = 
     using (new FileWriter(fileName)) {
       fileWriter => fileWriter.write(content)
     }
