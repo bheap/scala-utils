bb333b7513a36014e26f84197c5c91fd902a8218
rossputin <ross@bheap.co.uk>
2012-01-02 17:05:33 +0000

    improve variable name in ResourceUtils

--- src/main/scala/ResourceUtils.scala
+++ src/main/scala/ResourceUtils.scala
@@ -7,5 +7,5 @@ package com.bheap.scalautils
   * @author <a href="mailto:ross@bheap.co.uk">rossputin</a>
   * @since 1.0 */
 object ResourceUtils {
-  def using[A <: {def close(): Unit}, B](param: A)(f: A => B): B = try { f(param) } finally { param.close() }
+  def using[A <: {def close(): Unit}, B](ioResource: A)(f: A => B): B = try { f(ioResource) } finally { ioResource.close() }
 }
